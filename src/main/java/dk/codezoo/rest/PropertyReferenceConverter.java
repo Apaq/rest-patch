3ab256c93ecb24d6391e2a17abda37a9fe393085
Michael Krog <mic@apaq.dk>
2018-04-16 15:04:56 +0200

    Refactored packaged

--- src/main/java/dk/codezoo/rest/PropertyReferenceConverter.java
+++
@@ -1,9 +0,0 @@
-package dk.codezoo.rest;
-
-import java.util.Collection;
-
-@FunctionalInterface
-public interface PropertyReferenceConverter<T> {
-
-    public Collection<String> translate(T input);
-}
